0767ac91d920b7c318eede061746325a855a9306
Bumluck <matthewman216@gmail.com>
2025-01-25 11:03:05 -0500

    GameManager
    - Added GameManager
    - Created a scene for the Main Menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Bullet;
+    public GameObject Enemy;
+
+    public bool isPlayerAlive;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject playerInstance = Instantiate(Player, new Vector2(-13, 0), Quaternion.identity);
+        isPlayerAlive = true;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
